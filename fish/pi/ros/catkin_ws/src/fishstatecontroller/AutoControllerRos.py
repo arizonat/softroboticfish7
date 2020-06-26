2933d87f55ab2b913b26b7aedc0d33910d4fa208
jadorian <jadorian@mit.edu>
2020-06-26 13:29:04 -0500

    HSV settings reset

--- fish/pi/ros/catkin_ws/src/fishstatecontroller/AutoControllerRos.py
+++ fish/pi/ros/catkin_ws/src/fishstatecontroller/AutoControllerRos.py
@@ -26,7 +26,7 @@ class NaiveColorTargetTracker():
     # red stretches 2 bands in hsv
     # these values are for yellow, keeping the 2 bands for red in the future
     self.hsv_lower_lower = (14,55,55)
-    self.hsv_lower_upper = (150,255,235)
+    self.hsv_lower_upper = (30,255,235)
     self.hsv_upper_lower = self.hsv_lower_lower
     self.hsv_upper_upper = self.hsv_lower_upper
 
