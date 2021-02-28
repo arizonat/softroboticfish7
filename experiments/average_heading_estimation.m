fd0b618b24cefa4885653cf1e4cc427e8825977a
Levi Cai <cail@mit.edu>
2021-02-27 19:03:46 -0500

    Added a few more experiments for heading averaging, including what the ROS PID filter does maybe, though maybe not since it needs to operate on the derivative

--- experiments/average_heading_estimation.m
+++ experiments/average_heading_estimation.m
@@ -15,25 +15,41 @@ y_ball = sin(2*pi*ball_f*x+10);
 y = y_swim + y_ball; % y is the observed signal
 
 %% Attempt to estimate signal
+cutoff_f = ball_f*7; %swim_f*0.5;
+
+% Setup simple IIR filter
 y_diir = y(1); %exponential smoothing or infinite impulse response filter
-RC = 1/(2*pi*(swim_f*0.2));
+RC = 1/(2*pi*(cutoff_f));
 alpha = sample_T/(RC+sample_T);
 
+% Setup simple FIR filter
 y_fir = y(1); %moving windowed average, evenly weighted, or finite impulse response
-win = floor(swim_T/sample_T);
+%win = floor(swim_T/sample_T);
+win = floor((1/cutoff_f)/(sample_T*2));
+
+% Setup simple butterworth filter (used by the PID ROS node)
+order = 2;
+[bb, ba] = butter(order, cutoff_f/(sample_f/2));
+[y_b, z] = filter(bb, ba, y(1), zeros(1,order));
 
 for t=2:size(x,2)
     dyt = y(t) - y(t-1);
     
     % Update simple IIR filter
-    yt = alpha * y(t) + (1-alpha) * h(t-1);
+    yt = alpha * y(t) + (1-alpha) * y_diir(t-1);
     y_diir = [y_diir yt];
     
     % Update simple FIR filter
     yt_fir = sum(y(max(t-win,1):t))/min(win,t);
     y_fir = [y_fir yt_fir];
     
+    [yt_b, z] = filter(bb, ba, y(t), z);
+    y_b = [y_b yt_b];
 end
+
+sum(abs(y-y_diir))
+sum(abs(y-y_fir))
+sum(abs(y-y_b))
 
 
 %% Plot
@@ -42,7 +58,8 @@ figure;
 hold on;
 %plot(x,y_swim);
 plot(x,y_ball);
-plot(x,y);
+%plot(x,y);
 plot(x,y_diir);
 plot(x,y_fir);
-legend("target","signal","iir","fir")
+plot(x,y_b);
+legend("target","iir","fir","butter")
